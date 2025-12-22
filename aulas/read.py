3d83b59d1f1d6f52569e6711224118e0cce5b6d0
Your Name <you@example.com>
2025-12-22 12:16:28 -0300

    Pandas 02 - read

--- aulas/read.py
+++ aulas/read.py
@@ -4,3 +4,15 @@ import pandas as pd
 
 df = pd.read_csv("../data/clientes.csv")
 df
+# %%
+
+df.to_csv("clientes.csv")
+
+# %%
+
+df.to_parquet("clientes.parquet", index=False)
+
+# %%
+
+df.to_parquet("clientes.parquet", index=False)
+
