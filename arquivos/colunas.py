3d6d11627f533569d4b2191c121d5b9838e93df8
Your Name <you@example.com>
2026-01-06 11:31:16 -0300

    Up

--- arquivos/colunas.py
+++ arquivos/colunas.py
@@ -2,8 +2,29 @@
 
 import pandas as pd
 
-df = pd.read_csv("../data/transacoes.csv")
+df = pd.read_csv("../data/transacoes.csv", sep=";")
 df
 # %%
 
 df.shape
+# %%
+
+df.info(memory_usage='deep')
+# %%
+
+df.dtypes
+# %%
+
+renamed_columns = {
+    "qtdePontos": "qtPontos",
+    "descSistemaOrigem": "SistemaOrigem"
+    }
+# %%
+
+df.rename(columns=renamed_columns, inplace=True)
+# %%
+
+df[["IdCliente", "QtdePontos"]].tail(5)
+# %%
+
+df
