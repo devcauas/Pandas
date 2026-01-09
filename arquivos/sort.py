64fd1068b3d4a15ce474fe6dd95d925e9e17146d
Your Name <you@example.com>
2026-01-09 11:34:40 -0300

    sort - arquivo

---
+++ arquivos/sort.py
@@ -0,0 +1,26 @@
+#%%
+import pandas as pd
+
+clientes = pd.read_csv("../data/clientes.csv", sep=';')
+
+max_ponto = clientes["qtdePontos"].max()
+filtro = clientes["qtdePontos"] == max_ponto
+clientes[filtro]
+# %%
+
+(clientes.sort_values(by="qtdePontos", ascending=False)
+         .head(5)["idCliente"])
+# %%
+
+brinquedo = pd.DataFrame(
+    {
+      "nome": ["teo", "ana", "nah", "jose"],
+      "idade": [32, 43, 35, 42],
+      "salario": [2345, 4533, 3245, 4533],
+    }
+)
+
+brinquedo
+# %%
+
+brinquedo.sort_values(by=["salario", "idade"], ascending=[False, True])
